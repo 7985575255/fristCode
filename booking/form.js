b65e7f2e29341e96cdfdf744c055a854c8a2df49
7985575255 <arjun21217@gmail.com>
2023-01-21 21:36:38 +0530

    This commit is basicaly give delete funcnalty

--- booking/form.js
+++ booking/form.js
@@ -1,23 +1,28 @@
-
 let form=document.querySelector('.form');
 
-
-form.addEventListener('submit', (event)=>{
+form.addEventListener('submit', (event) =>{
     event.preventDefault();
-    let name=document.querySelector('.name').value;
-    let email=document.querySelector('#email').value;
-    let number=document.querySelector('#number').value;
-    let obj={
-        name,
-        email,
-        number
-    }
-    localStorage.setItem(obj.email, JSON.stringify(obj));
-    showOnscreen(obj);
+ let parentEle=document.querySelector("#userDeatils");
+let childEle=document.createElement('li');
+ let name=document.querySelector('.name').value;
+ let email=document.querySelector('#email').value;
+ let number=document.querySelector('#number').value;
+ let obj={
+    name,
+    email,
+    number
+ }
+ localStorage.setItem(obj.email, JSON.stringify(obj));
+childEle.innerText=JSON.stringify(obj);
+
+
+let delButton=document.createElement('input');
+delButton.type='button';
+delButton.value='delete';
+   delButton.onclick= ()=>{
+    localStorage.removeItem(obj.email);
+    parentEle.removeChild(childEle);
+   }
+childEle.appendChild(delButton);
+parentEle.appendChild(childEle);
 })
-function showOnscreen(obj){
-    let parentEle=document.querySelector('#userDeatils');
-    let childEle=document.createElement('li');
-    childEle.innerText=`${obj.name}-${obj.email}-${obj.number}`;
-    parentEle.appendChild(childEle);
-}
