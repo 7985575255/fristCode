9ec3251ec5b621f06c8be3c4ba6f66a667ce4e28
7985575255 <arjun21217@gmail.com>
2023-02-03 20:34:31 +0530

    This commit is basicaly Traker app

--- booking/form.js
+++ booking/form.js
@@ -1,39 +1,55 @@
-let form=document.querySelector('.form');
-
-form.addEventListener('submit', (event) =>{
-    event.preventDefault();
- let parentEle=document.querySelector("#userDeatils");
-let childEle=document.createElement('li');
- let name=document.querySelector('.name').value;
- let email=document.querySelector('#email').value;
- let number=document.querySelector('#number').value;
- let obj={
-    name,
-    email,
-    number
- }
- localStorage.setItem(obj.email, JSON.stringify(obj));
-childEle.innerText=JSON.stringify(obj);
+let form = document.querySelector('.form');
 
 
-let delButton=document.createElement('input');
-delButton.type='button';
-delButton.value='delete';
-   delButton.onclick= ()=>{
-    localStorage.removeItem(obj.email);
-    parentEle.removeChild(childEle);
+form.addEventListener('submit', (event) => {
+   event.preventDefault();
+   let parentEle = document.querySelector("#userDeatils");
+   let childEle = document.createElement('li');
+   let name = document.querySelector('.name').value;
+   let email = document.querySelector('#email').value;
+   let number = document.querySelector('#number').value;
+   let obj = { name, email, number }
+
+   localStorage.setItem(obj.email, JSON.stringify(obj));
+   childEle.innerText = JSON.stringify(obj);
+
+
+   let delButton = document.createElement('input');
+   delButton.type = 'button';
+   delButton.value = 'delete';
+   delButton.onclick = () => {
+      localStorage.removeItem(obj.email);
+      parentEle.removeChild(childEle);
    }
-   let editButton=document.createElement('input');
-   editButton.value='edit';
-   editButton.type='button';
-   editButton.onclick=()=>{
-    localStorage.removeItem(obj.email);
-    parentEle.removeChild(childEle);
-    document.querySelector('.name').value=obj.name;
-    document.querySelector('#email').value=obj.email;
-    document.querySelector('#number').value=obj.number;
+   let editButton = document.createElement('input');
+   editButton.value = 'edit';
+   editButton.type = 'button';
+   editButton.onclick = () => {
+      localStorage.removeItem(obj.email);
+      parentEle.removeChild(childEle);
+      document.querySelector('.name').value = obj.name;
+      document.querySelector('#email').value = obj.email;
+      document.querySelector('#number').value = obj.number;
    }
-childEle.appendChild(editButton);
-childEle.appendChild(delButton);
-parentEle.appendChild(childEle);
+
+   childEle.appendChild(editButton);
+   childEle.appendChild(delButton);
+   parentEle.appendChild(childEle);
+
 })
+   let parentEle = document.querySelector("#userDeatils");
+   let childEle = document.createElement('li');
+
+addEventListener('DOMContentLoaded', (event)=>{
+
+
+   const key=Object.keys(localStorage);
+   const arr=[];
+   key.forEach((key)=>arr.push(localStorage.getItem(key)));
+   
+      
+   childEle.innerText=arr
+   
+     parentEle.appendChild(childEle);
+})
+
